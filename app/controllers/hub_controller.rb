6a488c9720463d03efc2f3735277fc186b2c59f4
Ben Bleything <ben@bleything.net>
2006-08-17 22:10:12 +0000

    add address form

--- app/controllers/hub_controller.rb
+++ app/controllers/hub_controller.rb
@@ -1,2 +1,4 @@
 class HubController < ApplicationController
+  def index
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,26 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 end
+
+class TabularFormBuilder < ActionView::Helpers::FormBuilder
+  (field_helpers - %w(check_box radiobutton hidden_field)).each do |selector|
+    src = <<-END_SRC
+      def #{selector}(field, options = {})
+        @template.content_tag("tr",
+          @template.content_tag("td", field.to_s.humanize, :class => 'label') +
+          @template.content_tag("td", super, :class => 'field'))
+      end
+    END_SRC
+    class_eval src, __FILE__, __LINE__
+  end
+end
+
+def tabular_form_for(name, object = nil, options = nil, &proc)
+  concat("<table>", proc.binding)
+  form_for(name,
+           object,
+           (options || {}).merge(:builder => TabularFormBuilder),
+           &proc
+          )
+  concat("</table>", proc.binding)
+end
