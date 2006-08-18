fe4dab924afd19036e04858305e84b1fba8a85a3
Ben Bleything <ben@bleything.net>
2006-08-18 20:47:01 +0000

    integrate google map

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,4 +1,20 @@
 # Filters added to this controller will be run for all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
+  def prepare_map(options = {})
+    map = Cartographer::Gmap.new('map')
+    
+    # set up defaults
+    map.controls = [:type, :large]
+    map.type = :hybrid
+    map.center = [39.70718665682654, -94.482421875]
+    map.zoom = 4
+    
+    # override defaults with passed-in options
+    options.each do |k,v|
+      map.send "#{k}=", v
+    end
+    
+    return map
+  end
 end
--- app/controllers/hub_controller.rb
+++ app/controllers/hub_controller.rb
@@ -1,4 +1,5 @@
 class HubController < ApplicationController
   def index
+    @map = prepare_map
   end
 end
