3ca4c017e11c6d17602237fd211e6454f71e8868
Ben Bleything <ben@bleything.net>
2006-08-21 18:58:12 +0000

    test for centroid calculation

--- test/unit/address_test.rb
+++ test/unit/address_test.rb
@@ -1,10 +1,10 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class AddressTest < Test::Unit::TestCase
-  fixtures :addresses
+  fixtures :addresses, :geocodings
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  # centroid test is from the example given at http://www.saltspring.com/brochmann/math/centroid/centroid.html
+  def test_centroid
+    assert_equal [6.20973782772, 8.6579275905], addresses(:centroid).centroid
   end
 end
