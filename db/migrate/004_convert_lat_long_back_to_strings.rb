418787596168d70e4a552a69812cdb273fceff0a
Ben Bleything <ben@bleything.net>
2006-08-22 00:33:50 +0000

    progress indicators for migrations that geocode

--- db/migrate/004_convert_lat_long_back_to_strings.rb
+++ db/migrate/004_convert_lat_long_back_to_strings.rb
@@ -3,13 +3,21 @@ class ConvertLatLongBackToStrings < ActiveRecord::Migration
     change_column :geocodings, :latitude,  :string
     change_column :geocodings, :longitude, :string
     
-    Address.find_all.each {|a| a.regeocode! }
+    count = Address.count
+    Address.find_all.each_with_index {|a,i| 
+      puts "-- Geocoding address #{i+1} of #{count}"
+      a.regeocode! 
+    }
   end
 
   def self.down
     change_column :geocodings, :latitude,  :float
     change_column :geocodings, :longitude, :float
     
-    Address.find_all.each {|a| a.regeocode! }
+    count = Address.count
+    Address.find_all.each_with_index {|a,i| 
+      puts "-- Geocoding address #{i+1} of #{count}"
+      a.regeocode! 
+    }
   end
 end
